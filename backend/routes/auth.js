0c28133376dde137826aa5c5f16b62f2e04de0ba
kevincardona <kevincardona1@gmail.com>
2018-01-29 17:59:20 -0500

    finished simple signup and login. still need to verify accounts

---
+++ backend/routes/auth.js
@@ -0,0 +1,106 @@
+var MongoClient	= require('mongodb').MongoClient
+var MongoURL		= 'mongodb://localhost:27017/bulletin'
+var bcrypt			= require('bcryptjs')
+var jwt					= require('jsonwebtoken')
+
+//TODO: Hide in config
+var jwtsecret = 'Thismasecret'
+
+// validates and creates a new account
+var signup = function ( req, res ) {
+
+	//Check to make sure email and password are included
+	if (!req.body.email || !req.body.password)
+		return res.json({ success: false, message: "Insufficient Information" })
+	//Check to make sure that email ends with @purdue.edu
+	if (!req.body.email.endsWith('@purdue.edu'))
+		return res.json({ success: false, message: 'Invalid Purdue Email'})
+	//Check to make sure that the password is a proper length
+	if (req.body.password.length < 8)
+		return res.json({ success: false, message: 'Password is too short' })
+
+	MongoClient.connect(MongoURL, function(err, db) {
+		if (err)
+			return res.json({ success: false, message: 'Error connecting to database' })
+
+		//TODO: Bcrypt passwor
+
+		var users = db.collection('users')
+
+		users.find({ email: req.body.email }).toArray(function (err, result) {
+			if (err)
+				return res.json({ success: false, message: 'Error connecting to database' })
+
+			if (result.length === 0) {
+				//Encrypt the password before sending it to the database
+				bcrypt.hash(req.body.password, 10, function(err, hash) {
+					if (err)
+						return res.json({ success: false, message: 'Error encrypting password' })
+
+					var user = {
+						email: req.body.email,
+						password: hash,
+						verified: false,
+						created: new Date()
+					}
+
+					users.insert(user, function ( err, db ) {
+						if (err)
+							return res.json({ success: false, message: 'Error inserting user into database' })
+						else
+							return res.json({ success: true, message: 'Created new account' })
+					})
+				})
+			}
+
+			if (result.length != 0)
+				return res.json({ success: false, message: 'User with that email already exists' })
+		})
+	})
+}
+
+// validates and signs user in
+var login = function ( req, res ) {
+	if (!req.body.email || !req.body.password)
+		return res.json({ success: false, message: 'Insufficient login information' })
+
+	MongoClient.connect(MongoURL, function(err, db) {
+		var users = db.collection('users')
+		users.find({ email: req.body.email }).toArray(function(err, result) {
+			if (err)
+				return res.json({ success: false, message: 'Error connecting to database' })
+			if (result.length === 0)
+				return res.json({ success: false, message: 'Invalid login information' })
+			bcrypt.compare(req.body.password, result[0].password, function(err, match) {
+				if (err)
+					return res.json({ success: false, message: 'Error comparing passwords' })
+				console.log(result[0].password, req.body.password)
+				if (!match)
+					return res.json({ success: false, message: 'Invalid login information' })
+				else {
+					var tokenitems = {
+						_id: result[0]._id,
+						username: req.body.username
+					}
+					var token = jwt.sign( tokenitems , jwtsecret,  {
+						expiresIn: 86400 //24 Hours
+					});
+
+					return res.json({ success: true, message: 'Successfully logged in!', token: token });
+				}
+			})
+		})
+	})
+}
+
+// verifies a users token
+var authenticate = function ( req, res, next ) {
+}
+
+var functions = {
+	signup: signup,
+	login: login,
+	authenticate: authenticate
+}
+
+module.exports = functions
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,16 @@
 var router		= require('express').Router()
+var auth			= require('./auth')
+
+////// UNPROTECTED ROUTES //////
 
 router.get('/', (req,res) => {
 	res.send({ success: true, message: 'Successfully connected to server!' })
 })
+router.post('/signup', auth.signup)
+router.post('/login', auth.login)
+
+////// PROTECTED ROUTES //////
+
+router.use(auth.authenticate)
 
 module.exports = router
