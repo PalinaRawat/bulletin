98bc2c99299ffae6b06967ffe143bd9ac19c8e75
eads1 <eads1@purdue.edu>
2018-02-10 10:06:59 -0500

    9 Images on Home
    I can load 9 at a time. Still working on selecting them dynamically

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -6,15 +6,53 @@
       <router-link to="/contact" tag="a">Contact</router-link>
       <img src="../assets/icon.svg">
     </div>
-    <h1>{{ msg }}</h1>
-    <img :src = "getImageSource()">
-    <li v-for="item in items">
-      {{item.title}}
-    </li>
-    <figure class="figure">
-      <img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/cinderella.jpg">
-      <figcaption>Cinderella wearing European fashion of the mid-1860’s</figcaption>
-    </figure>
+    <h1>{{msg}}</h1>
+    <div id="columns">
+      <figure>
+        <img :src="listOfFlyers[0].image_url">
+        <p>
+          {{listOfFlyers[0].title}}
+        </p>
+      </figure>
+      <figure>
+        <img :src="listOfFlyers[1].image_url">
+        <p>
+          {{listOfFlyers[1].title}}
+        </p>
+      </figure>
+      <figure>
+        <img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/belle.jpg">
+        <figcaption>Belle, based on 1770’s French court fashion</figcaption>
+      </figure>
+      <figure>
+        <img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/mulan_2.jpg">
+        <figcaption>Mulan, based on the Ming Dynasty period</figcaption>
+      </figure>
+      <figure>
+        <img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/sleeping-beauty.jpg">
+        <figcaption>Sleeping Beauty, based on European fashions in 1485</figcaption>
+      </figure>
+      <figure>
+        <img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/pocahontas_2.jpg">
+        <figcaption>Pocahontas based on 17th century Powhatan costume</figcaption>
+      </figure>
+
+<figure>
+<img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/snow-white.jpg">
+<figcaption>Snow White, based on 16th century German fashion</figcaption>
+</figure>
+
+ <figure>
+<img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/ariel.jpg">
+<figcaption>Ariel wearing an evening gown of the 1890’s</figcaption>
+</figure>
+
+<figure>
+<img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/tiana.jpg">
+<figcaption>Tiana wearing the <i>robe de style</i> of the 1920’s</figcaption>
+</figure>
+
+</div>
   </div>
 </template>
 
@@ -25,20 +63,24 @@ export default {
   name: 'Welcome',
   data () {
     return {
-      msg: 'This is the home page.',
-      items: []
+      msg: 'Home Page',
+      url1: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Mallard2.jpg/1200px-Mallard2.jpg',
+      title1: 'broken',
+      listOfFlyers: [],
+      currentFlyers: [],
+      counter: 0
     }
   },
   created () {
     this.getflyers()
+    this.selectFlyers()
   },
   methods: {
     getflyers () {
       const context = this
       context.message = 'yoo'
       axios.post(`http://localhost:5000/getflyers`, this.credentials).then(res => {
-        context.imageSource = res.data.flyers[1].image_url
-        context.items = res.data.flyers
+        context.listOfFlyers = res.data.flyers
       })
         .catch(function (error) {
           context.msg = 'an error occurred.' + error
@@ -47,6 +89,14 @@ export default {
     getImageSource () {
       const context = this
       return context.imageSource
+    },
+    selectFlyers () {
+      const context = this
+      var i = 0
+      for (i = 0; i < 9; i++) {
+        context.currentFlyers[i] = context.listOfFlyers[context.counter]
+        context.counter++
+      }
     }
   }
 
@@ -85,5 +135,64 @@ figure {
 img {
   height: 4%;
   width: 4%;
+}
+body {
+  background: url(//subtlepatterns.com/patterns/scribble_light.png);
+  font-family: Calluna, Arial, sans-serif;
+  min-height: 1000px;
+}
+#columns {
+  column-width: 320px;
+  column-gap: 15px;
+  width: 90%;
+  max-width: 1100px;
+  margin: 50px auto;
+}
+
+div#columns figure {
+  background: #fefefe;
+  border: 2px solid #fcfcfc;
+  box-shadow: 0 1px 2px rgba(34, 25, 25, 0.4);
+  margin: 0 2px 15px;
+  padding: 15px;
+  padding-bottom: 10px;
+  transition: opacity .4s ease-in-out;
+  display: inline-block;
+  column-break-inside: avoid;
+}
+
+div#columns figure img {
+  width: 100%; height: auto;
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 15px;
+  margin-bottom: 5px;
+}
+
+div#columns figure p {
+  font-size: .9rem;
+  color: #444;
+  line-height: 1.5;
+}
+
+div#columns small {
+  font-size: 1rem;
+  float: right;
+  text-transform: uppercase;
+  color: #aaa;
+}
+
+div#columns small a {
+  color: #666;
+  text-decoration: none;
+  transition: .4s color;
+}
+
+div#columns:hover figure:not(:hover) {
+  opacity: 0.4;
+}
+
+@media screen and (max-width: 750px) {
+  #columns { column-gap: 0px; }
+  #columns figure { width: 100%; }
 }
 </style>
